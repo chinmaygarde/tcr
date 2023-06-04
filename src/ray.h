6e18b2dee29f5d1cc57a2e56265d26ebe133b9e1
Chinmay Garde <chinmaygarde@google.com>
2023-06-04 13:38:23 -0700

    Setup ray and types.

---
+++ src/ray.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "fml/macros.h"
+#include "types.h"
+
+namespace tcr {
+
+struct Ray {
+  Point origin;
+  Direction direction;
+};
+
+}  // namespace tcr
---
+++ src/types.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+#include "fml/macros.h"
+
+namespace tcr {
+
+using Point = glm::vec3;
+using Direction = glm::vec3;
+
+}  // namespace tcr
