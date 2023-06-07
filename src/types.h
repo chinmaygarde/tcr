81d1ce25ecf2024ae62c3b578287e92a542dfa13
Chinmay Garde <chinmaygarde@gmail.com>
2023-06-07 10:07:02 -0700

    Ray sphere intersection.

--- src/types.h
+++ src/types.h
@@ -1,12 +1,34 @@
 #pragma once
 
 #include <glm/glm.hpp>
+#include <glm/gtx/intersect.hpp>
 
 #include "fml/macros.h"
 
 namespace tcr {
 
+using Scalar = float;
 using Point = glm::vec3;
 using Direction = glm::vec3;
+
+struct Ray {
+  Point origin;
+  Direction direction;
+};
+
+struct Sphere {
+  Point center;
+  Scalar radius;
+
+  bool Intersects(const Ray& ray) const {
+    glm::vec3::value_type interection_distance;
+    return glm::intersectRaySphere(ray.origin,                     //
+                                   glm::normalize(ray.direction),  //
+                                   center,                         //
+                                   radius * radius,                //
+                                   interection_distance            //
+    );
+  }
+};
 
 }  // namespace tcr
